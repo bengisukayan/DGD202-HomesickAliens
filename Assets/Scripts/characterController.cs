77e99b997a5c3a253cca0238c0195596a3ac5d11
bengisukayan <bengisu.kayan@playersrift.com>
2024-06-11 16:36:20 +0300

    Player movement fixed

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -42,7 +42,7 @@ public class characterController : MonoBehaviour
 
     private void Update()
     {
-        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
+        if (_grounded && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
         {
             if (Input.GetKey(KeyCode.A))
             {
