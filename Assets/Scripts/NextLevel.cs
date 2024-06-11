1e4a985a5720b9b6f3d4dd8e2f10e716c6c92f5e
bengisukayan <bengisu.kayan@playersrift.com>
2024-06-11 17:06:55 +0300

    Finished Candy level

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -21,7 +21,7 @@ public class NextLevel : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") && Score.currentMoney == 1)
+        if (collision.gameObject.CompareTag("Player") && Score.currentMoney == 5)
         {
             StartLevel();
         }
